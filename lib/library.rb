36b6e9716b4cf2c6a44451abc5e85bd67c82b338
username <wnt925@gmail.com>
2019-05-22 15:57:17 -0600

    finish 3

--- lib/library.rb
+++ lib/library.rb
@@ -5,9 +5,39 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class Library
-  attr_reader :name
+  attr_reader :name, :books, :authors
 
   def initialize(name)
-    @name = name 
+    @name = name
+    @books = []
+    @authors = []
   end
+
+  def add_author(author)
+    @authors << author
+    author.books.each do |book|
+      @books << book
+    end
+
+    def add_books
+      @authors.map do |author|
+        author.books.map do |book|
+          @books << book
+        end
+      end
+    end
+
+
+  def publication_time_frame_for(author)
+    range_hash = Hash.new
+    range_array = []
+    author.books.each do |book|
+    range_array << book.publication_year
+      end
+    range_array.sort.reverse!
+    range_hash[:start] = range_array.last
+    range_hash[:end] = range_array.first
+  range_hash
+end
+end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -9,6 +9,11 @@ class LibraryTest < MiniTest::Test
   def setup
     @dpl = Library.new("Denver Public Library")
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    @professor = @charlotte_bronte.write("The Professor", "1857")
+    @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @villette = @charlotte_bronte.write("Villette", "1853")
+    @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
   end
 
   def test_it_exists
@@ -21,60 +26,33 @@ class LibraryTest < MiniTest::Test
     expected = "Denver Public Library"
     actual = @dpl.name
     assert_equal expected, actual
-    
-  assert_equal expected, actual
+    expected = []
+    actual = @dpl.books
+    assert_equal expected, actual
+    expected = []
+    actual = @dpl.authors
+    assert_equal expected, actual
   end
 
-  Use TDD to create a `Library` class that responds to the following interaction pattern.
+  def test_lib_can_add_authors_and_books
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+    @dpl.add_books
+    expected = [@charlotte_bronte, @harper_lee]
+    actual = @dpl.authors
+    assert_equal expected, actual
+    actual = @dpl.books
+    refute actual.empty?
+  end
 
-  The `publication_time_frame_for` method takes an `Author` object as an argument and returns a hash with two key/value pairs:
-    * `:start` which points to the publication year of the `Author`'s first book.
-    * `:end` which points to the publication year of the `Author`'s last book.
-
-
- dpl = Library.new("Denver Public Library")
-  #=> #<Library:0x00007fbeea3653c0...>
-
-  pry(main)> dpl.name
-  #=> "Denver Public Library"
-
-  pry(main)> dpl.books
-  #=> []
-
-  pry(main)> dpl.authors
-  #=> []
-
-  pry(main)> charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-  #=> #<Author:0x00007fbeea2d78b8...>
-
-  pry(main)> charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-  #=> #<Book:0x00007fbeeb3beca8...>
-
-  pry(main)> professor = charlotte_bronte.write("The Professor", "1857")
-  #=> #<Book:0x00007fbeea8efd90...>
-
-  pry(main)> villette = charlotte_bronte.write("Villette", "1853")
-  #=> #<Book:0x00007fbeea24fbe8...>
-
-  pry(main)> harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-  #=> #<Author:0x00007fbeea112730...>
-
-  pry(main)> mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
-  #=> #<Book:0x00007fbeeb1089f0...>
-
-  pry(main)> dpl.add_author(charlotte_bronte)
-
-  pry(main)> dpl.add_author(harper_lee)
-
-  pry(main)> dpl.authors
-  => [#<Author:0x00007fbeea2d78b8...>, #<Author:0x00007fbeea112730...>]
-
-  pry(main)> dpl.books
-  => [#<Book:0x00007fbeeb3beca8...>, #<Book:0x00007fbeea8efd90...>, #<Book:0x00007fbeea24fbe8...>, #<Book:0x00007fbeeb1089f0...>]
-
-  pry(main)> dpl.publication_time_frame_for(charlotte_bronte)
-  #=> {:start=>"1847", :end=>"1857"}
-
-  pry(main)> dpl.publication_time_frame_for(harper_lee)
-  #=> {:start=>"1960", :end=>"1960"}
-  ```
+  def test_pub_time_frame
+    @dpl.add_author(@harper_lee)
+    expected = {:start=>"1960", :end=>"1960"}
+    actual = @dpl.publication_time_frame_for(@harper_lee)
+    assert_equal expected, actual
+    @dpl.add_author(@charlotte_bronte)
+    expected = {:start=>"1853", :end=>"1857"}
+    actual = @dpl.publication_time_frame_for(@charlotte_bronte)
+    assert_equal expected, actual
+  end
+end
